347cf0f658a9852e3f79b5f980efe0eeff2f1d69
maryam <maryamfismaill1@gmail.com>
2024-02-26 12:44:44 -0500

    Initial Commit

---
+++ ResumeSwiftUI/ResumeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeSwiftUIApp.swift
+//  ResumeSwiftUI
+//
+//  Created by Maryam Ismail on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
